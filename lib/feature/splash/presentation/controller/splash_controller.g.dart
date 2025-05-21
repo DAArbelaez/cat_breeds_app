eebff72d877fc4121a74e8625facb6b18d242cdd
DAArbelaez <diegoandresarbelaez@gmail.com>
2025-05-20 21:03:27 -0500

    Add splash and routing using go_router

---
+++ lib/feature/splash/presentation/controller/splash_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'splash_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$splashControllerHash() => r'e726599453fd562cba18e129de362e38c96e3a14';
+
+/// See also [SplashController].
+@ProviderFor(SplashController)
+final splashControllerProvider =
+    AutoDisposeNotifierProvider<SplashController, void>.internal(
+      SplashController.new,
+      name: r'splashControllerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$splashControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SplashController = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
+import 'package:cat_breeds_app/core/navigation/go_router.dart';
+import 'package:cat_breeds_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
       title: 'Cat Breeds',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: const CatBreedsApp(),
+      theme: AppTheme.lightTheme,
+      routerConfig: goRouter,
     );
   }
 }
